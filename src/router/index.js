82984a06bef21d45ea76641cf4bf56bf76906389
Bradley Faircloth <bradleybsf12@gmail.com>
2023-02-20 17:13:24 -0500

    Fixed eslint issues

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import ProjectView from '../views/ProjectView.vue'
-import ResumeView from '../views/ResumeView.vue'
+import {createRouter, createWebHistory} from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AboutView from '../views/AboutView.vue';
+import ProjectView from '../views/ProjectView.vue';
+import ResumeView from '../views/ResumeView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
   },
   {
     path: '/about',
     name: 'about',
-    component: AboutView
+    component: AboutView,
   },
   {
     path: '/projects',
-    name: 'projects', 
-    component: ProjectView
+    name: 'projects',
+    component: ProjectView,
   },
   {
     path: '/resume',
     name: 'resume',
-    component: ResumeView
-  }
-]
+    component: ResumeView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -3,15 +3,13 @@
       <PersonalProjects/>
     </div>
   </template>
-  
-  <script>
-  // @ is an alias to /src
-  import PersonalProjects from '@/components/PersonalProjects.vue'
-  
-  export default {
-    name: 'ProjectView',
-    components: {
-      PersonalProjects
-    }
-  }
-  </script>
+<script>
+import PersonalProjects from '@/components/PersonalProjects.vue';
+
+export default {
+  name: 'ProjectView',
+  components: {
+    PersonalProjects,
+  },
+};
+</script>
