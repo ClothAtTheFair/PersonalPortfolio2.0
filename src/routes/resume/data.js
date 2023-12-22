b4bd92a4a0b1e79aef58966ff89112389bd657cc
Bradley Faircloth <bradleybsf12@gmail.com>
2023-12-21 23:40:22 -0500

    routing based on svelte tutorial

---
+++ src/routes/resume/data.js
@@ -0,0 +1,26 @@
+export const posts = [
+	{
+		slug: 'work',
+		title: 'Work Experience',
+		content:
+            '<p>TBD</p>'
+	},
+
+	{
+		slug: 'education',
+		title: 'Education',
+		content:
+			'<p>TBD</p>'
+	},
+
+	{
+		slug: 'skills',
+		title: 'Training, Certifications, and Skills',
+		content: "<p>TBD</p>"
+	},
+    {
+        slug: 'awards',
+        title: 'Awards',
+        content: "<p>TBD</p>"
+    }
+];
