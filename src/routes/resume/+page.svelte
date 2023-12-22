efaef825f37bcb16f97f6845ebb84c30865d7186
Bradley Faircloth <bradleybsf12@gmail.com>
2023-12-21 23:40:22 -0500

    routing based on svelte tutorial

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let data;
+</script>
+
+<h1>Professional Resume</h1>
+<h3>For a downloadable pdf of the information, click here</h3>
+
+<ul>
+	{#each data.summaries as { slug, title }}
+		<li><a href="/resume/{slug}">{title}</a></li>
+	{/each}
+</ul>
