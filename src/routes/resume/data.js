6014db4dc523a71a176b8c94c4034ced9fd73bf4
Bradley Faircloth <bradleybsf12@gmail.com>
2023-12-25 23:15:19 -0500

    Update infomation for subroutes

--- src/routes/resume/data.js
+++ src/routes/resume/data.js
@@ -3,20 +3,30 @@ export const posts = [
 		slug: 'work',
 		title: 'Work Experience',
 		content:
-            '<p>TBD</p>'
+            '<h2><b>Bosch, Controls Engineer</b></h2><p>April 2023 - Present</p>\
+			<p> Test Engineer covering Mechanical, Electrical, and Software engineering maintenance and improvements for over 18 automated stations. </p> <br>\
+			<h2><b>Booz Allen Hamilton, Lead Engineer</b></h2><p> October 2022 - April 2023</p> \
+			<p> Assisted with integration and testing of software products within the Marine Corps Intelligence sector.</p><br>\
+			<h2><b>JHU Applied Physics Lab, Autonomy T&E Software Engineer</b></h2><p> May 2021 - October 2022 </p> \
+			Created and lead quality testing and good DevOp practices in various projects for Naval sponsors.'
 	},
 
 	{
 		slug: 'education',
 		title: 'Education',
 		content:
-			'<p>TBD</p>'
+			'<h2><b>Masters of Science, Computer Science</b></h2> <p>Johns Hopkins University - Baltimore</p>\
+			  <p> August 2021 - Present </p>\
+			  <br><h2><b>Bachelor of Science, Mechanical Engineering</b></h2> <p>University of South Carolina - Columbia</p>\
+			  <p> August 2014 - May 2019</p>'
 	},
 
 	{
 		slug: 'skills',
 		title: 'Training, Certifications, and Skills',
-		content: "<p>TBD</p>"
+		content: "<b>Certifications:</b> CompTIA Security+ <br>\
+					<b>Programming:</b> Java; C/C++; C#/.Net; JavaScript; MATLAB; Python; ReactJS; Clojure; NodeJS; SvelteKit; Rust <br>\
+					<b>Management Software</b>: Gatsby;  Jenkins;  Kubernetes;  xunit/junit;  Docker;  Git; Travis; Azure;</b>"
 	},
     {
         slug: 'awards',
