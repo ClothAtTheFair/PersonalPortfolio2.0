34ca5fec9ed3c04786517f5b10dc33a6c59d961a
Bradley Faircloth <bradleybsf12@gmail.com>
2023-12-26 23:13:31 -0500

    Added google form for contacting

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,10 @@
+<div class="socialMedia" aria-label="socialButtons">
+    <a href="https://twitter.com/BradleyFairclo2" target="_blank" rel="noopener noreferrer" ><i class="fab fa-twitter"></i>X</a>
+    <a href="https://github.com/ClothAtTheFair" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i>GitHub</a>
+    <a href="https://www.linkedin.com/in/bradley-faircloth-576a74a3/" target="_blank" rel="noopener noreferrer" ><i class="fab fa-linkedin"></i>LinkedIn</a>
+    <a href="https://amazing-babbage-a08cab.netlify.com/" target="_blank" rel="noopener noreferrer"><i class="fas fa-scroll"></i>Blog</a>
+    </div>
+
+<iframe title="contact-form" 
+src="https://docs.google.com/forms/d/e/1FAIpQLScS9acfbqXTcfQfp1PvtYaunrfsu2EuRZ0yVKvzhJBqOPJaXg/viewform?embedded=true" width="600" 
+height="800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
